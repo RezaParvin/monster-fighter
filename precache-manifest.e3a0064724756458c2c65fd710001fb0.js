b87644a80f5093841cb19a6feba1301717cafd57
{RezaParvin} <{rezaparvin338@gmail.com}>
2020-05-18 12:59:05 +0430

    Updates

---
+++ precache-manifest.e3a0064724756458c2c65fd710001fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493c3f725040aee54826c64ef2df4a90",
+    "url": "/monster-fighter/index.html"
+  },
+  {
+    "revision": "1dbe4fbfd0d165146f39",
+    "url": "/monster-fighter/static/css/main.627eed88.chunk.css"
+  },
+  {
+    "revision": "0916bde0eb7e736fa955",
+    "url": "/monster-fighter/static/js/2.b35c41c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-fighter/static/js/2.b35c41c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dbe4fbfd0d165146f39",
+    "url": "/monster-fighter/static/js/main.b589edf8.chunk.js"
+  },
+  {
+    "revision": "4c59516331759e677415",
+    "url": "/monster-fighter/static/js/runtime-main.85db7697.js"
+  }
+]);
